75af11c219b7af65bda611cbc5dbcd1408981fed
magarmon12 <136698052+magarmon12@users.noreply.github.com>
2025-02-19 22:38:07 +0000

    added.env

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # class-PHP-project
+A php sample project for **Advanced Web** class 2025
+
+#Configuration
+rename *.env.dist* tto *.env* and change values to suit your environment
--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
-include "header.php";
+
+use Twig\Loader\FilesystemLoader;
+
+require_once 'vendor/autoload.php';
+//testing phpdotenv
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__); 
+$dotenv->load();
+
+//create data variables
+$page_title = "Class Project Home Page";
+$greeting ="Welcome to my website";
+
+/*include "header.php";
 echo"<h1 style='color: blue;'> Hello World</h1>";
-include "footer.php";
+include "footer.php";*/
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig =new \Twig\Environment( $loader);
+$template = $twig -> load('page.twig');
+//render the output
+echo $template -> render( [ 'title' => $page_title, 'greeting' => $greeting ] );
+
 ?>
